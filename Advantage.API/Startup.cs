0784e17e0139a23e3dfd675b6b407d9fd602f17e
unknown <egkarahan@gmail.com>
2019-06-22 22:47:02 +0300

    Controllers added

--- Advantage.API/Startup.cs
+++ Advantage.API/Startup.cs
@@ -50,11 +50,13 @@ namespace Advantage.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
+
             seed.SeedData(20, 1000);
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc(routes => routes.MapRoute(
+                "default", "api/{controller}/{action}/{id}"
+             ));
 
             
         }
